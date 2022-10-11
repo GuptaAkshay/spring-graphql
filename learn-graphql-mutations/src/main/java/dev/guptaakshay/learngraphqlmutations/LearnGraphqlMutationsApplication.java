f8f47f83f1b8e4eec64dfaf88ea0988d03024c3d
Akshay Gupta <akshgupta547@gmail.com>
2022-10-11 22:47:40 +0530

    adding code for mutations

--- learn-graphql-mutations/src/main/java/dev/guptaakshay/learngraphqlmutations/LearnGraphqlMutationsApplication.java
+++ learn-graphql-mutations/src/main/java/dev/guptaakshay/learngraphqlmutations/LearnGraphqlMutationsApplication.java
@@ -1,7 +1,15 @@
 package dev.guptaakshay.learngraphqlmutations;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class LearnGraphqlMutationsApplication {
@@ -11,3 +19,25 @@ public class LearnGraphqlMutationsApplication {
 	}
 
 }
+
+@Controller
+class MutationController {
+
+	private final Map<Integer, Customer> db = new ConcurrentHashMap<>();
+	private final AtomicInteger id = new AtomicInteger();
+
+	@MutationMapping
+	Customer addCustomer(@Argument String name) {
+		var id = this.id.incrementAndGet();
+		var value = new Customer(id, name);
+		this.db.put(id, value);
+		return value;
+	}
+
+	@QueryMapping
+	Customer customerById(@Argument Integer id) {
+		return this.db.get(id);
+	}
+}
+
+record Customer(Integer id, String name) {}
