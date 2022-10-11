c06a7d4cf0aaa58a08fa9ad2686a5ef1c7314252
Akshay Gupta <akshgupta547@gmail.com>
2022-10-11 11:49:25 +0530

    graphql queries using spring-graphql & reactive web

---
+++ learn-graphql-queries/src/test/java/dev/guptaakshay/learngraphqlqueries/LearnGraphqlQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqlqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
