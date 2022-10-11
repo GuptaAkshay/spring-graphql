b962aea95c8b2124e50fb8bb2f0dad6682d58cf8
Akshay Gupta <akshgupta547@gmail.com>
2022-10-11 11:48:27 +0530

    basic graphql configs, how it works/wire!

---
+++ learn-graphql/src/test/java/dev/guptaakshay/learngraphql/LearnGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
