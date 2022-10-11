7cc05326b50bc35374094017f298569657dbd057
Akshay Gupta <akshgupta547@gmail.com>
2022-10-11 22:53:17 +0530

    client init code

---
+++ learn-graphql-clients/src/main/java/dev/guptaakshay/learngraphqlclients/LearnGraphqlClientsApplication.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqlclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGraphqlClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGraphqlClientsApplication.class, args);
+	}
+
+}
---
+++ learn-graphql-clients/src/test/java/dev/guptaakshay/learngraphqlclients/LearnGraphqlClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqlclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
