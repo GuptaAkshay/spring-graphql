50f5f2708c03abca9a1e5c444eb1411812f84647
Akshay Gupta <akshgupta547@gmail.com>
2022-10-11 21:12:33 +0530

    graphql mutation init

---
+++ learn-graphql-mutations/src/main/java/dev/guptaakshay/learngraphqlmutations/LearnGraphqlMutationsApplication.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqlmutations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGraphqlMutationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGraphqlMutationsApplication.class, args);
+	}
+
+}
---
+++ learn-graphql-mutations/src/test/java/dev/guptaakshay/learngraphqlmutations/LearnGraphqlMutationsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqlmutations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlMutationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
