cb954432d0709fbe08c799b930365a9eabd97ccd
Akshay Gupta <akshgupta547@gmail.com>
2022-10-12 23:00:44 +0530

    graphql securiy

---
+++ learn-graphql-security/src/test/java/dev/guptaakshay/learngraphqlsecurity/LearnGraphqlSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqlsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
