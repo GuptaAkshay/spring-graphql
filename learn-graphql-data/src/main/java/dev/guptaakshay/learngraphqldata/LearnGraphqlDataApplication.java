88b6e2f2f6bcb6c89ad42382ef43ed2ba2d17b79
Akshay Gupta <akshgupta547@gmail.com>
2022-10-12 23:06:16 +0530

    init for graphql data

---
+++ learn-graphql-data/src/main/java/dev/guptaakshay/learngraphqldata/LearnGraphqlDataApplication.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGraphqlDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGraphqlDataApplication.class, args);
+	}
+
+}
---
+++ learn-graphql-data/src/test/java/dev/guptaakshay/learngraphqldata/LearnGraphqlDataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guptaakshay.learngraphqldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
