c7c7e99d1cc4f4784e1cf2bb15d103b7d0cfab73
Akshay Gupta <akshgupta547@gmail.com>
2022-10-12 22:14:16 +0530

    graphql subscription via rsocket

--- learn-graphql-subscription/src/main/java/dev/guptaakshay/learngraphqlsubscription/LearnGraphqlSubscriptionApplication.java
+++ learn-graphql-subscription/src/main/java/dev/guptaakshay/learngraphqlsubscription/LearnGraphqlSubscriptionApplication.java
@@ -1,7 +1,17 @@
 package dev.guptaakshay.learngraphqlsubscription;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
+import org.springframework.stereotype.Controller;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class LearnGraphqlSubscriptionApplication {
@@ -11,3 +21,21 @@ public class LearnGraphqlSubscriptionApplication {
 	}
 
 }
+
+@Controller
+class GreetingController {
+
+	@SubscriptionMapping
+	Flux<Greeting> greetings(){
+		return Flux.fromStream(Stream.generate(() -> new Greeting("Hello, World @ "+ Instant.now() + "!")))
+			.delayElements(Duration.ofSeconds(1))
+			.take(10);
+	}
+
+	@QueryMapping
+	Greeting greeting (){
+		return new Greeting("Hello, World!");
+	}
+}
+
+record Greeting(String greeting) {}
