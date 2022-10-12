80bb441256f4aa04e6f4ac5e6ec4dcd3c9574563
Akshay Gupta <akshgupta547@gmail.com>
2022-10-12 23:52:36 +0530

    graphql data via query dsl and mongodb

--- learn-graphql-data/src/main/java/dev/guptaakshay/learngraphqldata/LearnGraphqlDataApplication.java
+++ learn-graphql-data/src/main/java/dev/guptaakshay/learngraphqldata/LearnGraphqlDataApplication.java
@@ -1,7 +1,21 @@
 package dev.guptaakshay.learngraphqldata;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.graphql.data.GraphQlRepository;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import com.querydsl.core.annotations.QueryEntity;
+
+import dev.guptaakshay.learngraphqldata.QCustomer;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class LearnGraphqlDataApplication {
@@ -10,4 +24,50 @@ public class LearnGraphqlDataApplication {
 		SpringApplication.run(LearnGraphqlDataApplication.class, args);
 	}
 
+/*	@Bean
+	RuntimeWiringConfigurer runtimeWiringConfigurer(CustomerRepository customerRepository) {
+		return new RuntimeWiringConfigurer() {
+			@Override
+			public void configure(RuntimeWiring.Builder builder) {
+				var data = QuerydslDataFetcher.builder(customerRepository).many();
+				var dataum = QuerydslDataFetcher.builder(customerRepository).single();
+				builder.type("Query", wiring -> wiring
+					.dataFetcher("customer", dataum)
+					.dataFetcher("customers", data));
+			}
+		};
+	}*/
+
+	@Bean
+	ApplicationRunner applicationRunner(CustomerRepository customerRepository) {
+		return args -> {
+
+			Flux<Customer> all = customerRepository.findAll(QCustomer.customer.name.startsWith("A").not());
+
+			customerRepository.deleteAll()
+				.thenMany(Flux.just("Akshay", "Ridhhi", "Harsh", "Shubham", "Anup")
+					.map(name -> new Customer(null, name))
+					.flatMap(customerRepository::save))
+				.thenMany(all)
+				.subscribe(System.out::println);
+		};
+	}
 }
+
+@Controller
+class ProfileController {
+
+	@SchemaMapping(typeName = "Customer")
+	Profile profile (Customer customer) {
+		return new Profile(customer.id());
+	}
+}
+
+@GraphQlRepository
+interface CustomerRepository extends ReactiveCrudRepository<Customer, String>, ReactiveQuerydslPredicateExecutor<Customer> { }
+
+@QueryEntity
+@Document
+record Customer(@Id String id, String name) {}
+
+record Profile(String id) {}
